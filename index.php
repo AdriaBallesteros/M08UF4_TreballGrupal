8fd4cf12449e1c628a99a5f0ffaa2a046b88258f
Adria <Adria@Adria-PC>
2020-09-30 19:57:38 +0200

    abb - Arreglado echo abb_funcion1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             function abb_funcion1(){
-                echo "funcion1";
+                echo "Adria Ballesteros";
             }
             
             function JMC(){
